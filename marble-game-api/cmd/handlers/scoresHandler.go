05832cd21190c3efdc6d3ec7c822519d7d1d1665
Jae <giffordjacob0@gmail.com>
2024-10-29 09:45:42 -0700

    i think i finally formatted the go files correctly

--- marble-game-api/cmd/handlers/scoresHandler.go
+++ marble-game-api/cmd/handlers/scoresHandler.go
@@ -2,18 +2,21 @@ package handlers
 
 import (
 	"fmt"
+	storage "marble-game-api/cmd/database"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func GetScores (c echo.Context) error {
 	fmt.Println("Entry")
-
-	return c.String(200, "Done")
-
-/* 	db := storage.GetDB()
-
-	psqlStatement := `SELECT TOP 10 FROM scores`
+	
+	db := storage.GetDB()
+	if db == nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Database connection failed")
+	}
+	
+	psqlStatement := `SELECT * FROM scores LIMIT 10`
 
 	rows, err := db.Query(psqlStatement)
 	fmt.Println(err, rows)
@@ -25,5 +28,5 @@ func GetScores (c echo.Context) error {
 	fmt.Println(rows)
 	var scores = rows
 	fmt.Println(http.StatusOK, scores)
-	return c.JSON(http.StatusOK, scores) */
+	return c.JSON(http.StatusOK, scores) 
 }
--- marble-game-api/main.go
+++ marble-game-api/main.go
@@ -3,31 +3,34 @@ package main
 import (
 	"marble-game-api/cmd/handlers"
 
-	//	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Debug = true
+	e.Use(middleware.Recover())
+
 	e.GET("/", handlers.Home)
 
 	// connect to DB
 	// storage.InitDB()
 
 	// starting routes
-/* 	e.POST("/users", handlers.CreateUser)
-	e.POST("/measurements", handlers.CreateMeasurement)
-	e.PUT("/users/:id", handlers.UpdateUser)
-	e.PUT("/measurements/:id", handlers.UpdateMeasurement) */
+	/* 	e.POST("/users", handlers.CreateUser)
+	   	e.POST("/measurements", handlers.CreateMeasurement)
+	   	e.PUT("/users/:id", handlers.UpdateUser)
+	   	e.PUT("/measurements/:id", handlers.UpdateMeasurement) */
 
 	e.GET("/scores", handlers.GetScores)
-//	e.Use(handlers.LogRequest)
-//	e.Use(middleware.Logger)
-/* e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	AllowOrigins: []string{"http://localhost:3000"},
-	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-  })) */
+	//	e.Use(handlers.LogRequest)
+	//	e.Use(middleware.Logger)
+	/* e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	  })) */
 	// start server or log fatal
 	e.Logger.Fatal(e.Start(":8080"))
-	
+
 }
